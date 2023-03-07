ff43c510595fcb57fd52732652146c1597e395bf
João Lourenção <jotap.lourencao@gmail.com>
2023-03-07 02:13:53 -0300

    update

--- src/data/services/facebook-authentication.ts
+++ src/data/services/facebook-authentication.ts
@@ -1,18 +1,22 @@
 import { type LoadFacebookUserApi } from '@/data/contracts/apis'
-import { type LoadUserAccountRepository } from '@/data/contracts/repos'
+import { type CreateFacebookAccountRepository, type LoadUserAccountRepository } from '@/data/contracts/repos'
 import { AuthenticationError } from '@/domain/errors'
 import { type FacebookAuthentication } from '@/domain/features'
 
 export class FacebookAuthenticationService {
   constructor(
     private readonly loadFacebookUserApi: LoadFacebookUserApi,
-    private readonly loadUserAccountRepository: LoadUserAccountRepository
+    private readonly loadUserAccountRepository: LoadUserAccountRepository,
+    private readonly createFacebookAccountRepository: CreateFacebookAccountRepository
   ) { }
 
   async exec(params: FacebookAuthentication.Params): Promise<AuthenticationError> {
     const fbData = await this.loadFacebookUserApi.loadUser(params)
 
-    if (fbData !== undefined) { await this.loadUserAccountRepository.load({ email: fbData.email }) }
+    if (fbData !== undefined) {
+      await this.loadUserAccountRepository.load({ email: fbData.email })
+      await this.createFacebookAccountRepository.createFromFacebook(fbData)
+    }
 
     return new AuthenticationError()
   }
